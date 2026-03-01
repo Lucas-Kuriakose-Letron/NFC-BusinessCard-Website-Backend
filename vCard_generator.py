6e58a3fffbd714b86be96e8d90d0ab67582dc19f
Lucas Kuriakose <lucas.kuriakose@gmail.com>
2026-03-01 14:49:43 -0500

    Update vCard_generator.py

--- vCard_generator.py
+++ vCard_generator.py
@@ -1,10 +1,16 @@
 class ContactCardGenerator:
     def generate_vcard(self, name, phone, company):
-        return f"""BEGIN:VCARD
-        VERSION:3.0
-        FN:{name}
-        ORG:{company}
-        TEL;TYPE=CELL:{phone}
-        END:VCARD
-        """
+        parts = name.strip().split(" ", 1)
+        first = parts[0]
+        last = parts[1] if len(parts) >1 else ""
+        return (
+            "BEGIN:VCARD\r\n"
+            "VERSION:3.0\r\n"
+            "N:" + last + ";" + first + ";;;\r\n"
+            "FN:" + name + "\r\n"
+            "ORG:" + company + "\r\n"
+            "TEL;TYPE=CELL:" + phone + "\r\n"
+            "END:VCARD\r\n"
+        )
+
     
