09d548277f435b93cc4e24c617fc9076f1426a10
Lucas Kuriakose <lucas.kuriakose@gmail.com>
2026-03-01 15:17:28 -0500

    Update App.py

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify, send_file, Response
 from flask_cors import CORS
 import json
 import os
@@ -234,3 +234,4 @@ def chat():
 if __name__ == "__main__":
 
     app.run(debug=True)
+
